e9cace2331c6ea4ba5e9ceb10315b94ffc06a57f
Yorely <yorely1200@gmail.com>
2022-07-05 01:02:58 -0500

    commit nosepudo

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from distutils.command.config import config
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 from config import config
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 conexion = MySQL(app)
@@ -17,11 +18,19 @@ def bd():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/grafica')
+def grafica():
     cursor = conexion.connection.cursor()
     sql = "SELECT id, duration, protocol_type, service, flag, src_bytes, class FROM prueba WHERE class = 'anomaly'"
+    #sql1 = "SELECT class FROM prueba WHERE class = 'normal'"
+    #sql2 = "SELECT class FROM prueba WHERE class = 'anomaly'"
     cursor.execute(sql)
     datos = cursor.fetchall()
     print(datos)
+
     return render_template('index.html')
 
 
