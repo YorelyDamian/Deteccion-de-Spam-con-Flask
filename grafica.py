5709a97380000fa5ad798614a32ce7cfcfde0ca4
Yorely <yorely1200@gmail.com>
2022-07-10 14:56:30 -0500

    commit "DatosGit"

--- grafica.py
+++ grafica.py
@@ -4,31 +4,27 @@ import matplotlib.pyplot as plt
 data = pd.read_csv(
     'D:\\Yorely\Windows\\Octavo Semestre\\Zamora\\Inteligencia Artificial\\proyecto_Spam\\ArchivosCSV\\normal.csv', delimiter=',')
 data.head()
-w = data['id']
-x = data['src_bytes']
+
 y = data['classN']
 z = data['classA']
 
-sumw = sum(w)
-sumx = sum(x)
+
 sumy = sum(y)
 sumz = sum(z)
 
 can = 125973
 por = 1000
 
-divw = sumw / can / por
-divx = sumx / can / por
+
 divy = sumy / can * 100
 divz = sumz / can * 100
 
-print('duration:', divw)
-print('src_bytes:', divx)
+
 print('classN:', sumy)
 print('classA:', sumz)
 
-porcentajes = [divw, divx, divy, divz]
-nombres = ["Duration", "src_Bytes", "Spam", "Normal"]
+porcentajes = [divy, divz]
+nombres = ["Spam", "Normal"]
 plt.pie(porcentajes, labels=nombres)
 
 plt.show()
